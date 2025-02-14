7e43bcf68c7454aa79dce6c8a2df19e6669a0ddc
Y1shuiLi <98449352+liyishui2003@users.noreply.github.com>
2025-02-14 21:51:13 +0800

    Create client.cpp

---
+++ 04_封装我们的第一个类/client.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include "util.h"
+
+#define BUFFER_SIZE 1024 
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == -1, "socket create error");
+
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(8888);
+
+    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect error");
+    
+    while(true){
+        char buf[BUFFER_SIZE];  //在这个版本，buf大小必须大于或等于服务器端buf大小，不然会出错，想想为什么？
+        /*
+        尝试回答：ET模式下服务器可以一次性尽可能读取客户端数据直到结束
+        但当服务器向客户端写数据时，这里的写法是一次性读入的
+        如果buf大小小于服务器端buf大小，数据就会被截断
+        服务器没法向客户端写完整的数据
+        */
+        bzero(&buf, sizeof(buf));
+        scanf("%s", buf);
+        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+        if(write_bytes == -1){
+            printf("socket already disconnected, can't write any more!\n");
+            break;
+        }
+        bzero(&buf, sizeof(buf));
+        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+        if(read_bytes > 0){
+            printf("message from server: %s\n", buf);
+        }else if(read_bytes == 0){
+            printf("server socket disconnected!\n");
+            break;
+        }else if(read_bytes == -1){
+            close(sockfd);
+            errif(true, "socket read error");
+        }
+    }
+    close(sockfd);
+    return 0;
+}
